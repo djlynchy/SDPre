aa9d1f17b1753d9db3e6207e612cb40cacaa6580
Derek Lynch <lynchderekjo@yahoo.com>
2022-10-04 15:21:25 -0600

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 12;
+    double side2 = 21;
+    double side3 = Math.sqrt((side1 * side1)+(side2 * side2));
+
+
+    System.out.println("The Hypotenuse of a triange with" + side1 + "and" + side2 + "is" + side3);
+  }
+}
