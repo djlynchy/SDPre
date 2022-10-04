4fcbde95b0fdb8e910b55f990abb8bcbc81f9e28
Derek Lynch <lynchderekjo@yahoo.com>
2022-10-04 13:32:00 -0600

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year_born = 1984;
+    String month_born = "November";
+    boolean weekend = false || false;
+
+    System.out.println(year_born);
+    System.out.println(month_born);
+    System.out.println(weekend);
+  }
+}
