92cf84bf0ad0b3542c14db9ed788478b9addb616
Derek Lynch <lynchderekjo@yahoo.com>
2022-10-04 14:26:32 -0600

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 9;
+    double num2 = 21;
+    double num3 = 42;
+
+
+    System.out.println((num1 + num2 + num3) / 3);
+  }
+}
