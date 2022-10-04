a24b6604c215d877d5c668eab3527dcfd555fbae
Derek Lynch <lynchderekjo@yahoo.com>
2022-10-04 12:01:52 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Derek Lynch";
+    int age = 37;
+    String state = "New Mexico";
+
+
+    System.out.println("Name:" + name + "Age:" + age + "State:" + state);
+
+
+  }
+}
