b0a488c24fb94500781b865f6c166ff1be9c101f
Derek Lynch <lynchderekjo@yahoo.com>
2022-10-06 12:20:45 -0600

    completed Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 0, sum = 0;
+
+    while (counter <= 20) {
+      sum += counter;
+      
+    counter = counter +1;
+    System.out.println(counter);
+  }
+
+    System.out.println("= " + sum);
+    sum = counter;
+  }
+  }
